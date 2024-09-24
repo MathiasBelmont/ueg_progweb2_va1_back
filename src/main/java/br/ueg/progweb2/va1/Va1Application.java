c6a96bf2641d9b60483d15198b2a5b89b03b8ced
Isaque Loures Valadão <102473956+crisper2001@users.noreply.github.com>
2024-09-24 13:18:12 -0300

    Commit inicial
    Inclusão dos arquivos gerados pelo Spring Initializr

---
+++ src/main/java/br/ueg/progweb2/va1/Va1Application.java
@@ -0,0 +1,13 @@
+package br.ueg.progweb2.va1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Va1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Va1Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/ueg/progweb2/va1/Va1ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ueg.progweb2.va1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Va1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
